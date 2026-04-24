458cc400d6693c847b0a46e57ad75f51bbf35e0f
Nicodemus Lin <nicodemusdemuse@gmail.com>
2026-04-24 08:21:31 +0700

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let map = new Map();
+
+        for (let i = 0; i < strs.length; i++) {
+            let str = strs[i];
+
+            let temp = new Array(26).fill(0);
+            for (let s of str) {
+                let index = s.charCodeAt(0) - 97;
+                temp[index]++;
+            }
+
+            console.log(str, temp.join(";"))
+
+            let find = temp.join(";");
+            let list = map.get(find) || [];
+            list.push(str);
+            map.set(find, list);
+        }
+
+        return [...map.values()];
+    }
+}
