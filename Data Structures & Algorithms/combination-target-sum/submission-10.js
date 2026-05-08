2cbfe89dd7789a8c2a92639e1b657f785ed40aa8
Nicodemus Lin <nicodemusdemuse@gmail.com>
2026-05-08 07:56:33 +0700

    Add: combination-target-sum - submission-10

---
+++ Data Structures & Algorithms/combination-target-sum/submission-10.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @returns {number[][]}
+     */
+    combinationSum(nums, target) {
+        nums.sort((a, b) => a - b);
+        let result = [];
+
+        let traverse = (target, memo, index) => {
+            console.log(target, memo, index);
+            // base
+            if (target === 0) {
+                // found match!
+                result.push([...memo]);
+                return;
+            } else if (target < 0) {
+                // failed
+                return;
+            }
+
+            for (let i = index; i < nums.length; i++) {
+                if (nums[i] > target) break;
+
+                memo.push(nums[i]);
+                traverse(target - nums[i], memo, i);
+
+                // backtrack
+                memo.pop();
+            }
+        };
+
+        traverse(target, [], 0);
+
+        return result;
+    }
+}
