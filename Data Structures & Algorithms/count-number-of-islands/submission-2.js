644e457a9c96ec418b073448f6261f5d5d76f42f
Nicodemus Lin <nicodemusdemuse@gmail.com>
2026-05-25 08:25:25 +0700

    Add: count-number-of-islands - submission-2

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-2.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {character[][]} grid
+     * @return {number}
+     */
+    numIslands(grid) {
+        let dfs = (x, y) => {
+            if (grid[x] === undefined || grid[x][y] === undefined || grid[x][y] === "0") return;
+
+            grid[x][y] = "0";
+
+            dfs(x, y + 1);
+            dfs(x + 1, y);
+            dfs(x, y - 1);
+            dfs(x - 1, y);
+        };
+
+        let result = 0;
+
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] === "1") {
+                    result++;
+                    dfs(i, j);
+                }
+            }
+        }
+
+        return result;
+    }
+}
