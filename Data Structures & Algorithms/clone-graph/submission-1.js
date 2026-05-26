edf6b9ec06cfb341aca1b265f53f75c7982f7db4
Nicodemus Lin <nicodemusdemuse@gmail.com>
2026-05-26 07:16:30 +0700

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.js
@@ -0,0 +1,36 @@
+/**
+ * // Definition for a Node.
+ * class Node {
+ *     constructor(val = 0, neighbors = []) {
+ *       this.val = val;
+ *       this.neighbors = neighbors;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Node} node
+     * @return {Node}
+     */
+    cloneGraph(node) {
+        if (node === null) return null;
+
+        let myMap = new Map();
+
+        let dfs = (currNode) => {
+            if (myMap.get(currNode) !== undefined) return myMap.get(currNode);
+
+            let newNode = new Node(currNode.val);
+            myMap.set(currNode, newNode);
+
+            for (let neighbor of currNode.neighbors) {
+                newNode.neighbors.push(dfs(neighbor));
+            }
+
+            return newNode;
+        };
+
+        return dfs(node);
+    }
+}
