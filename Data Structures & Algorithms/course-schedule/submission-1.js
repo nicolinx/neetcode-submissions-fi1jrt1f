9d098ce14fb0feac4596d8947705e3192e0eb5ab
Nicodemus Lin <nicodemusdemuse@gmail.com>
2026-05-29 07:22:41 +0700

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.js
@@ -0,0 +1,46 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {boolean}
+     */
+    canFinish(numCourses, prerequisites) {
+        let adjList = Array.from({ length: numCourses }, () => []);
+
+        for (let [a, b] of prerequisites) {
+            adjList[b].push(a);
+        }
+
+        let visited = Array(numCourses).fill(false);
+        let inStack = Array(numCourses).fill(false);
+
+        // check cycle
+        let dfs = (course) => {
+            // THIS IS A CYCLE
+            if (inStack[course] == true) return true;
+
+            // ALREADY VISITED
+            if (visited[course] == true) return false;
+
+            inStack[course] = true;
+
+            for (let neighbor of adjList[course]) {
+                if (dfs(neighbor) == true) return true;
+            }
+
+            // BACKTRACK
+            inStack[course] = false;
+
+            // MARK IT AS VISITED
+            visited[course] = true;
+            
+            return false;
+        };
+
+        for (let i = 0; i < numCourses; i++) {
+            if (dfs(i) == true) return false;
+        }
+
+        return true;
+    }
+}
