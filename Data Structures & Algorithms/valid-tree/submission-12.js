235bcaa7dbb672621f9e8d15d11f62c199d69327
Nicodemus Lin <nicodemusdemuse@gmail.com>
2026-06-01 14:41:09 +0700

    Add: valid-tree - submission-12

---
+++ Data Structures & Algorithms/valid-tree/submission-12.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {boolean}
+     */
+    validTree(n, edges) {
+        // create adjlist
+        let adjList = Array.from({ length: n }, () => []);
+
+        for (let [a, b] of edges) {
+            adjList[a].push(b);
+            adjList[b].push(a);
+        }
+
+        let visited = Array(n).fill(false);
+
+        // cycle validator
+        let dfs = (node, parent) =>{
+            visited[node] = true;
+
+            for(let neighbor of adjList[node]){
+                if(neighbor === parent) continue;
+
+                if(visited[neighbor] === true) return true;
+
+                if(dfs(neighbor, node) === true) return true;
+            }
+
+            return false;
+        }
+
+        if(dfs(0, -1) === true) return false;
+
+        // Every visited must be true, too verify all nodes have connections/lines
+        return visited.filter(e => e === false).length > 0 === false;
+    }
+}
