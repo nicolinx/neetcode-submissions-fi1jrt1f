71cc9bf80db343c17d12f28296732401eba37abf
Nicodemus Lin <nicodemusdemuse@gmail.com>
2026-06-03 14:38:30 +0700

    Add: three-integer-sum - submission-14

---
+++ Data Structures & Algorithms/three-integer-sum/submission-14.js
@@ -0,0 +1,49 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        nums.sort((a, b) => a - b);
+
+        console.log("Nums: ", nums);
+
+        let set = new Set();
+
+        for (let i = 0; i < nums.length; i++) {
+            let j = 0;
+            let k = nums.length - 1;
+            let target = -nums[i];
+
+            while (j < k) {
+                // console.log(nums[i], nums[j], nums[k]);
+
+                if (j === i) {
+                    j++;
+                    continue;
+                }
+                if (k === i) {
+                    k--;
+                    continue;
+                }
+
+                let sum = nums[j] + nums[k];
+
+                // console.log("PASS, target: ", target, " SUM: ", sum);
+
+                if (sum < target) j++;
+                else if (sum > target) k--;
+                else {
+                    let save = [nums[i], nums[j], nums[k]].sort();
+                    set.add(save.join(","));
+                    j++;
+                    k--;
+                }
+            }
+        }
+
+        // console.log(set);
+
+        return Array.from(set, (str) => str.split(",").map(Number));
+    }
+}
