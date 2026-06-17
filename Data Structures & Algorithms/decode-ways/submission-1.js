09c8650f48f1edb9f8f0b0a33cc9b5a3f5e85db3
Nicodemus Lin <nicodemusdemuse@gmail.com>
2026-06-17 07:49:08 +0700

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    numDecodings(s) {
+        let memo = {};
+        let count = (i) => {
+            if(memo[i] !== undefined) return memo[i];
+
+            if (i === s.length) return 1;
+
+            if (s[i] === "0") return 0;
+
+            let res = count(i + 1);
+
+            if (i < s.length - 1) {
+                let s2 = s[i] + s[i + 1];
+                if (Number(s2) <= 26) {
+                    res += count(i + 2);
+                }
+            }
+
+            memo[i] = res;
+
+            return res;
+        };
+
+        return count(0);
+    }
+}
