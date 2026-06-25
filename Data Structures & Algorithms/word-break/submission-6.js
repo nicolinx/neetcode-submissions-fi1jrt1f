851222a617b53f6229879eec09f17a958c8ad62c
Nicodemus Lin <nicodemusdemuse@gmail.com>
2026-06-26 06:46:34 +0700

    Add: word-break - submission-6

---
+++ Data Structures & Algorithms/word-break/submission-6.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string[]} wordDict
+     * @return {boolean}
+     */
+    wordBreak(s, wordDict) {
+        let wordSet = new Set();
+        for (let str of wordDict) {
+            wordSet.add(str);
+        }
+
+        let memo = {};
+
+        let count = (i) => {
+            if(memo[i] !== undefined) return memo[i];
+
+            if (i === s.length) return true;
+
+            for (let x = i; x < s.length; x++) {
+                let str = s.substring(i, x + 1);
+                if (wordSet.has(str)) {
+                    if (count(x + 1) === true) return true;
+                }
+            }
+
+            memo[i]= false;
+            return false;
+        };
+
+        return count(0);
+    }
+}
