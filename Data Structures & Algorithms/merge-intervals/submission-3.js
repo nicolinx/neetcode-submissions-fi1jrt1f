e058d5964618d693bd543412ef73431f35a43c8b
Nicodemus Lin <nicodemusdemuse@gmail.com>
2026-07-21 08:09:07 +0700

    Add: merge-intervals - submission-3

---
+++ Data Structures & Algorithms/merge-intervals/submission-3.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number[][]}
+     */
+    merge(intervals) {
+        intervals.sort((a, b) => a[0] - b[0]);
+
+        let result = [];
+        result.push(intervals[0]);
+
+        for (let i = 1; i < intervals.length; i++) {
+            let last = result[result.length - 1];
+            if (last[1] >= intervals[i][0]) {
+                result[result.length - 1] = [
+                    Math.min(last[0], intervals[i][0]),
+                    Math.max(last[1], intervals[i][1]),
+                ];
+            }else{
+                result.push(intervals[i]);
+            }
+        }
+
+        return result;
+    }
+}
