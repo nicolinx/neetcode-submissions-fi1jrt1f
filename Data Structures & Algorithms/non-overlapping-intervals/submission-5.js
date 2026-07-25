9f20cef5e607efba31f69a503de0cdd0f96825dd
Nicodemus Lin <nicodemusdemuse@gmail.com>
2026-07-25 14:54:19 +0700

    Add: non-overlapping-intervals - submission-5

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-5.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number}
+     */
+    eraseOverlapIntervals(intervals) {
+        intervals.sort((a, b) => a[1] - b[1]);
+
+        let removeCount = 0;
+        let prevEnd = -Infinity;
+
+        for(let i = 0; i< intervals.length;i++){
+            if(intervals[i][0] < prevEnd){
+                removeCount++;
+            }else{
+                prevEnd = intervals[i][1];
+            }
+        }
+
+        return removeCount;
+    }
+}
