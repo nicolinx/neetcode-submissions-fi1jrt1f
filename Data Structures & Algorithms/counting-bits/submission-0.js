855cef9bc9e84094e3da311cb0939bf0dd0eb634
Nicodemus Lin <nicodemusdemuse@gmail.com>
2026-08-09 17:30:39 +0700

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number[]}
+     */
+    countBits(n) {
+        let dp = new Array(n + 1).fill(0);
+        let offset = 1;
+
+        for (let i = 1; i <= n; i++) {
+            if (offset * 2 === i) {
+                offset = i;
+            }
+            dp[i] = 1 + dp[i - offset];
+        }
+
+        return dp;
+    }
+}
