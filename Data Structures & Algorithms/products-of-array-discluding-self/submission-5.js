0b21de1fbfaca1e90ed580d669a76270588f68d9
Nicodemus Lin <nicodemusdemuse@gmail.com>
2026-08-13 09:15:18 +0700

    Add: products-of-array-discluding-self - submission-5

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-5.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let n = nums.length;
+        let res = new Array(nums.length).fill(1);
+
+        for (let i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+
+        let a = 1;
+
+        for (let i = n - 2; i >= 0; i--) {
+            a = a * nums[i + 1];
+            res[i] = res[i] * a;
+        }
+
+        return res;
+    }
+}
