56414c5f89b690d2484f61d5e11ad90bb575abde
Nicodemus Lin <nicodemusdemuse@gmail.com>
2026-08-14 17:57:39 +0700

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let i = 0;
+        let j = s.length - 1;
+
+        const isAlphanumeric = (str) => /^[a-z0-9]+$/i.test(str);
+
+        while (i < j) {
+            if (!isAlphanumeric(s[i])) {
+                i++;
+                continue;
+            }
+            if (!isAlphanumeric(s[j])) {
+                j--;
+                continue;
+            }
+
+            if (s[i].toLowerCase() !== s[j].toLowerCase()) return false;
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
